58225894fac03d74276ed93bda430c3791758a21
My Computer <My Computer@134.154.41.25>
2019-02-27 13:10:07 -0800

    master

---
+++ src/lab6.java
@@ -0,0 +1,8 @@
+
+public class lab6 {
+
+public static void main(String args)
+{
+	System.out.println("Version 1");
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author My Computer
+ *
+ */
+module lab6 {
+}
